7e504768ed4f8063e1705ae638bd752c4d09d2e8
chernichenko <alexchernichenko18@gmail.com>
2024-12-21 11:21:31 +0200

    Updates

--- assets/main-_8iwlTKn.js
+++ assets/main-_8iwlTKn.js
@@ -36,17 +36,20 @@
   }
 })();
 (() => {
-  var _a, _b;
+  var _a, _b, _c;
   const menuRefs = {
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closeMenuBtn: document.querySelector("[data-menu-close]"),
+    overlay: document.querySelector("[data-menu-overlay]"),
     menu: document.querySelector("[data-menu]")
   };
   (_a = menuRefs.openMenuBtn) == null ? void 0 : _a.addEventListener("click", toggleMenu);
   (_b = menuRefs.closeMenuBtn) == null ? void 0 : _b.addEventListener("click", toggleMenu);
+  (_c = menuRefs.overlay) == null ? void 0 : _c.addEventListener("click", toggleMenu);
   function toggleMenu() {
-    var _a2;
+    var _a2, _b2;
     (_a2 = menuRefs.menu) == null ? void 0 : _a2.classList.toggle("is-open");
+    (_b2 = menuRefs.overlay) == null ? void 0 : _b2.classList.toggle("is-open");
   }
   const header = document.querySelector("[data-header]");
   window.addEventListener("scroll", () => {
@@ -56,4 +59,17 @@
       header == null ? void 0 : header.classList.remove("scrolled");
     }
   });
+  const menuItems = document.querySelectorAll(".navigation-link");
+  function setActiveMenu() {
+    const currentHash = window.location.hash;
+    menuItems.forEach((item) => {
+      if (item.getAttribute("href") === currentHash) {
+        item.classList.add("active");
+      } else {
+        item.classList.remove("active");
+      }
+    });
+  }
+  window.addEventListener("hashchange", setActiveMenu);
+  window.addEventListener("load", setActiveMenu);
 })();
